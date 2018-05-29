64bb4efe8df2100415838ebc3512b91aceeb2102
Justin Parry <justinsaiparry@gmail.com>
2018-05-29 13:16:57 -0600

    test commit

---
+++ a06/a06/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  a06
+//
+//  Created by Justin Parry on 5/29/18.
+//  Copyright © 2018 jEnoch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
