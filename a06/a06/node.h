847f0cf1b21341137193bd7320fac553be3c53d5
Justin Parry <justinsaiparry@gmail.com>
2018-05-29 21:52:31 -0600

    finished node

--- a06/a06/node.h
+++ a06/a06/node.h
@@ -31,7 +31,7 @@ namespace custom
       /****************************************
        * INTERFACES
        ****************************************/
-      node <T> * insert(node <T> * pNode, const T & e, bool after = false) throw (const char *);
+      node <T> * insert(node <T> * pCurrent, const T & e, bool after = false) throw (const char *);
       node <T> * find(node <T> * pHead, const T & e);
       void clear(node <T> * & pHead);
       node <T> * copy(const node <T> * pSource) throw (const char *);
@@ -52,11 +52,119 @@ namespace custom
    /****************************************
     * INTERFACES
     ****************************************/
+   
+   /*****************************
+    * INSERT
+    *
+    *****************************/
    template <class T>
-   node <T> :: node <T> * insert(node <T> * pNode, const T & e, bool after = false) throw (const char *)
+   node <T> node <T> :: * insert(node <T> * pCurrent, const T & e, bool after = false) throw (const char *)
    {
+      node <T> * pNew = new node <T> (e);
       
+      if (pCurrent != NULL && after == false)
+      {
+         pNew -> pNext = pCurrent;
+         pNew -> pPrev = pCurrent -> pPrev;
+         pCurrent -> pPrev = pNew;
+         
+         if (pNew -> pPrev)
+         {
+            pNew -> pPrev -> pNext = pNew;
+         }
+      }
+      
+      if (pCurrent != NULL && after == true)
+      {
+         pNew -> pNext = pCurrent -> pNext;
+         pNew -> pPrev = pCurrent;
+         pCurrent -> pNext = pNew;
+      
+         if (pNew -> pNext)
+         {
+            pNew -> pNext -> pPrev = pNew;
+         }
+      }
+   } // insert
+   
+   /*****************************
+    * CLEAR
+    *
+    *****************************/
+   template <class T>
+   void node <T> :: clear(node <T> * & pHead)
+   {
+      while (pHead != NULL)
+      {
+         node <T> pDelete = pHead;
+         pHead = pHead -> pNext;
+         delete pDelete;
+      }
+   } // clear
+   
+   /*****************************
+    * COPY
+    *
+    *****************************/
+   template <class T>
+   node <T> node <T> :: * copy(const node <T> * pSource) throw (const char *)
+   {
+      node <T> * pDestiny = new node <T> (pSource -> data);
+      node <T> pSrc = pSource;
+      node <T> pDes = pDestiny;
+      
+      for (node <T> * p = pSrc; p; p = pSrc -> pNext)
+      {
+         pDes = insert(pDes, pSrc -> data, true);
+      }
+      
+      return pDestiny;
+   } // COPY
+   
+   /*****************************
+    * FIND
+    *
+    *****************************/
+   template <class T>
+   node <T> node <T> :: * find(node <T> * pHead, const T & e)
+   {
+      for (node <T> * p = pHead; p; p = p -> pNext)
+      {
+         if (p -> data == e)
+         {
+            return p;
+         }
+         
+         return NULL;
+      }
    }
+   
+   /*****************************
+    * REMOVE
+    *
+    *****************************/
+   template <class T>
+   node <T> node <T> :: * remove (const node <T> * pRemove)
+   {
+      if (NULL == pRemove)
+         return;
+      
+      if (pRemove -> pPrev)
+         pRemove -> pPrev -> pNext = pRemove -> pNext;
+      if (pRemove -> pPrev)
+         pRemove -> pNext -> pPrev = pRemove -> pPrev;
+      
+      node <T> * pReturn = NULL;
+      
+      if (pRemove -> pPrev)
+         pReturn = pRemove -> pPrev;
+      else
+         pReturn = pRemove -> pNext;
+      
+      delete pRemove;
+      return pReturn;
+   } // ReMOVE
+   
    
 }// custom
 
